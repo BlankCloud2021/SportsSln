6fc0799d078df09364a9bcb00173ce54c7c2e0aa
Kent Moseley <moseleykent856@gmail.com>
2021-04-27 09:24:30 -0400

    Add project files.

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository _repository;
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            _repository = repo;
+        }
+        public IViewComponentResult Invoke() 
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            var model = _repository.Products.Select(x => x.Category).Distinct().OrderBy(x =>x);
+
+            return View(model);
+        }
+
+    }
+}
---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IStoreRepository _storeRepository;
+        public int PageSize = 4;
+        public HomeController(IStoreRepository storeRepository)
+        {
+            _storeRepository = storeRepository;
+        }
+
+        public IActionResult Index(string category ,int productPage = 1)
+        {
+            var model = new ProductsListViewModel
+            {
+                Products = _storeRepository.Products.Where(p => p.Category == category || category == null).OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = category == null ?
+                    _storeRepository.Products.Count() :
+                    _storeRepository.Products.Where(e =>
+                    e.Category == category).Count()
+                }
+            }; 
+            return View(model);
+        }
+    }
+}
